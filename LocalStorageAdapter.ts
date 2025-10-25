9c0dc3ee245331a37371bb3541c97b27861dab72
Konstantin Ladutenko <kostyfisik@gmail.com>
2025-10-25 17:28:24 +0300

    fix

---
+++ LocalStorageAdapter.ts
@@ -0,0 +1,15 @@
+import { StoragePort } from './StoragePort.js';
+
+export class LocalStorageAdapter implements StoragePort {
+  getItem(key: string): string | null {
+    return localStorage.getItem(key);
+  }
+
+  setItem(key: string, value: string): void {
+    localStorage.setItem(key, value);
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+}
